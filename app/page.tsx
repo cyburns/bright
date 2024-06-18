59874bc32c55aa4487562fe491d6919d307cce35
cyburns <72774499+cyburns@users.noreply.github.com>
2024-06-18 09:23:37 -0400

    relative path

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Hero from "@/components/home/Hero";
-import About from "@/components/home/About";
-import Contact from "@/components/home/Contact";
-import Works from "@/components/home/Works";
-import Services from "@/components/home/Services";
-import InfiniteWords from "@/components/home/infinite";
+import Hero from "../components/home/Hero";
+import About from "../components/home/About";
+import Contact from "../components/home/Contact";
+import Works from "../components/home/Works";
+import Services from "../components/home/Services";
+import InfiniteWords from "../components/home/infinite";
+
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
